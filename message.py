da0c4623e72db3407b8772e14d98395fbcb66cd0
EdouardDeWulf <e.dewulf@student.uclouvain.be>
2019-11-29 16:58:01 +0100

    pression joystick pour selection et confirmer, suppression parties inutiles, changement couleur lors selection, stop si confirm, chiffres en cycle

--- message.py
+++ message.py
@@ -6,7 +6,8 @@ x = 0 #Valeur initial du compteur = 0
 sense = SenseHat()
 Message = [] # Liste de stockage des caractère du message
 key= "chocolat" # Clé du chiffrement vigenere
-sense.low_light = True
+sense.low_light = True 
+lock = True #False si un message est déjà enregistré
 
 def encode(key, plain_text ): #Fonction chiffrant le message selon le chiffrement vigenere
     
@@ -15,7 +16,7 @@ def encode(key, plain_text ): #Fonction chiffrant le message selon le chiffremen
         key_c = key[i % len(key)]
         enc_c = chr((ord(e) + ord(key_c)) % 256)
         enc.append(enc_c)
-    return ("".join(enc))#.encode()).decode()
+    return ("".join(enc))#.encode()).decode() Dans la librairie mais fonctionne seulement sans
     
 def decode(key, cipher_text): #Fonction déchiffrant le message selon le chiffrement vigenere
     
@@ -37,60 +38,39 @@ def value(value, min_value=0, max_value=9): #Fonction limitant la valeur des car
     return min(max_value, max(min_value, value))
 
 
-def pushed_up(event): #Fonction augmentant la valeur
+def Plus(event): #Fonction augmentant la valeur
     global x
     if event.action != ACTION_RELEASED:
         x = value(x + 1)
         sense.show_letter(str(x))
         
 
-def pushed_down(event): #Fonction diminuant la valeur
+def Moins(event): #Fonction diminuant la valeur
     global x
     if event.action != ACTION_RELEASED:
         x = value(x - 1)
         sense.show_letter(str(x))
 
-        
-def pushed_left(event): #Fonction faisant appel à la fonction "encode" pour écrire le message chiffré dans .txt
+def Confirm(event): #Press ajoute la lettre. Hold confirme le message et ferme le programme 
     global Message
-    global key
-    if event.action != ACTION_HELD:
-        f = open("message.txt", "w")
-        f.write(encode(key,"".join(Message)))
-        f.close()
-        sense.clear()
-        
-#Enlever les # pour test. la fonction devrait renvoyer le message en clair dans la console        
-#    if event.action != ACTION_RELEASED:
-#      print (decode(key,encode(key,Message)))
-#      sense.clear()
+    global lock
+    if event.action != ACTION_PRESSED:
+        if lock :
+            if event.action == ACTION_RELEASED:
+                sense.show_letter(str(x),(0, 255, 0)) #le caractère devient vert ( confirmation )
+                Message.append(str(x)) #ajout du caractère à la string Message
+                sleep(0.2)
+                sense.show_letter(str(x)) #le caractère redevient blanc
+            else: #Longue pression 
+                f = open("message.txt", "w") #ouvre un .txt
+                f.write(encode(key,"".join(Message))) #écrit le message chiffré 
+                f.close()
+                sense.clear()
+                lock = False #Permet d'enregistrer qu'une fois
 
-def pushed_right(event): #Fonction ajoutant un caractère au message
-  global x
-  global Message
-  if event.action != ACTION_RELEASED:
-      sense.show_letter(str(x),(0, 255, 0)) #le caractère devient vert ( confirmation )
-      Message.append(str(x)) #ajout du caractère à la string Message
-      sleep(0.2)
-      sense.show_letter(str(x)) #le caractère redevient blanc
-      
-#Objectif : Pression Joystick pour ajouter un caractère, longue pression pour finir le message.
-        #   Remplacerait donc le Joystick vers la gauche et droite
-#Problème : incapacité de définir une longue pression.
-#def confirm(event):
-#    global Message
-#    if event.action != ACTION_HELD:
-#      f = open("message.txt", "w")
-#      f.write(encode(key,"".join(Message)))
-#      f.close()
-#    if event.action != ACTION_RELEASED:
-#        Message.append[str(x)]
-    
-
-sense.stick.direction_up = pushed_up #Joystick vers le haut : valeur +1
-sense.stick.direction_down = pushed_down #Joystick vers le bas : valeur -1
-sense.stick.direction_left = pushed_left #Joystick vers la gauche : chiffrement et .txt
-sense.stick.direction_right = pushed_right #Joystick vers la droite : ajout d'un caractère au message
-#sense.stick.direction_middle = confirm
+sense.stick.direction_up = Plus #Joystick vers le haut : valeur +1
+sense.stick.direction_down = Moins #Joystick vers le bas : valeur -1
+sense.stick.direction_left = Moins #Joystick vers la gauche : chiffrement et .txt
+sense.stick.direction_right = Plus #Joystick vers la droite : valeur +1
+sense.stick.direction_middle = Confirm #press ajout au message hold confirmer
 pause()
-
