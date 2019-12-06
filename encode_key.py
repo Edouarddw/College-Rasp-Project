ece8f980cf5c40246b21ae0b03fd6bf82df7fe08
EdouardDeWulf <e.dewulf@student.uclouvain.be>
2019-12-06 15:42:54 +0100

    adaptations pour compatibilité avec message.py + bug confirmations

--- encode_key.py
+++ encode_key.py
@@ -1,8 +1,8 @@
 from sense_hat import SenseHat
 from subprocess import call
 
-s = SenseHat()
-s.low_light = True
+sense = SenseHat()
+sense.low_light = True
 
 def hashing(string): #fonction hachant la clef
     
@@ -26,17 +26,15 @@ def hashing(string): #fonction hachant la clef
 
 liste_action = [] #liste de stockage des positions dans l espace
 action = 0
-s.show_message("Encode",scroll_speed = 0.05)
+sense.show_message("Encode",scroll_speed = 0.05)
 tourne = True
-conserver = True
-validation = True
 while tourne :
-    s.show_letter(str(len(liste_action)))
-    event = s.stick.wait_for_event()
-    if event.action == "released" and event.direction == "middle" : #pression sur le joystick pour ajouter une position
-        x = round(s.get_accelerometer_raw()["x"])
-        y = round(s.get_accelerometer_raw()["y"])
-        z = round(s.get_accelerometer_raw()["z"])
+    sense.show_letter(str(len(liste_action)))
+    event = sense.stick.wait_for_event()
+    if event.action == "pressed" and event.direction == "middle" : #pression sur le joystick pour ajouter une position
+        x = round(sense.get_accelerometer_raw()["x"])
+        y = round(sense.get_accelerometer_raw()["y"])
+        z = round(sense.get_accelerometer_raw()["z"])
         if y == 0 and x == 0 and z == 1 :
             action = "Nothing"
             liste_action.append(action)
@@ -62,30 +60,34 @@ while tourne :
             action = "flipbackward"
             liste_action.append(action) 
     if event.action == "held" and event.direction == "middle" :
-        s.show_message("Valider?",scroll_speed = 0.05)
-        while validation : 
-          while conserver :
-              s.show_letter("V",(0, 255, 0))
-              for event in s.stick.get_events(): # Si on valide en appuyant au milieu, le message sera conserve, une autre action proposera le x
-                  if event.action == 'released' and event.direction == "middle":
-                      tourne = False
-                      conserver = False
-                      validation = False
-                      s.clear()
-                      f= open("key.txt","w") #ouvre le document message.txt
-                      f.write(hashing("".join(liste_action))) #ecrit la clef hashee
-                      f.close()
-                      call("python3 message.py", shell=True) #lance message.py
-                  if event.action == "released" and event.direction != "middle" :
-                      conserver = False
-                      delete = True
-          while delete :
-              s.show_letter("X",(255, 0, 0))
-              for event in s.stick.get_events(): # Si on valide en appuyant au milieu, le message sera supprime, une autre action recommencera la bouche
-                  if event.action == 'released' and event.direction == "middle":
-                      delete = False
-                      validation = False
-                      s.clear()
-                  if event.action == "released" and event.direction != "middle" :
-                      conserver = True
-                      delete = False
+        sense.show_message("Valider?",scroll_speed = 0.05)
+        conserver = True
+        validation = True
+        delete = False
+        while validation :
+            while conserver :
+                sense.show_letter("V",(0, 255, 0))
+                for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera conserve, une autre action proposera le x
+                    if event.action == 'pressed' and event.direction == "middle":
+                        tourne = False
+                        conserver = False
+                        validation = False
+                        sense.clear()
+                        f= open("key.txt","w") #ouvre le document message.txt
+                        f.write(hashing("".join(liste_action))) #ecrit la clef hashee
+                        f.close()
+                        call("python3 message.py", shell=True) #lance message.py
+                    if event.action == "pressed" and event.direction != "middle" :
+                        conserver = False
+                        delete = True
+            while delete :
+                sense.show_letter("X",(255, 0, 0))
+                for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera supprime, une autre action recommencera la bouche
+                    if event.action == 'pressed' and event.direction == "middle":
+                        delete = False
+                        validation = False
+                        sense.clear()
+                        liste_action = []
+                    if event.action == "pressed" and event.direction != "middle" :
+                        conserver = True
+                        delete = False
