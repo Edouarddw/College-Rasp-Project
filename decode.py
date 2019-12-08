311fa2cb5f903c5f15094beb5503e944631dd6a8
EdouardDeWulf <e.dewulf@student.uclouvain.be>
2019-12-08 22:13:26 +0100

    implementation du module VX dans decode.py

--- decode.py
+++ decode.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat
 from subprocess import call
+import module
 sense = SenseHat()
 sense.clear()
 key= "chocolat" #clef introduite lors de l encodage
@@ -28,56 +29,18 @@ while tourne :
       if event.action == 'pressed':
         sense.clear()
         tourne = False
-tourne = True 
-conserver = True
-sup = True 
+
 sense.show_message("Conserver?",scroll_speed = 0.05)
-while tourne :
-  while conserver :
-    sense.show_letter("V",(0, 255, 0))
-    for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera conserve, une autre action proposera le x 
-      if event.action == 'pressed' and event.direction == "middle":
-        tourne = False
-        conserver = False
-        sense.clear()
+a = module.vx() #VX est le return du choix entre V ou X
+if a :
+    call("sudo shutdown now", shell=True) # V = stop le rasp
+else : # X = message supprime, une autre action recommencera la boucle
+    #permet de choisir entre enregistrer un a nouveau clef et message ou non si non, eteint le rasp
+    sense.show_message("Nouveau?",scroll_speed = 0.05)
+    b = module.vx() #VX est le return du choix entre V ou X
+    if b : call("python3 encode_key.py", shell=True) #lance l encodage d une nouvelle clef
+    else :
+        f= open("message.txt","w") #ouvre le document message.txt
+        f.write("") #remplace par un message vide donc supprime
+        f.close()
         call("sudo shutdown now", shell=True) #stop le rasp
-      if event.action == "pressed" and event.direction != "middle" :
-        conserver = False
-        delete = True
-  while delete :
-    sense.show_letter("X",(255, 0, 0))
-    for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera supprime, une autre action recommencera la bouche 
-      if event.action == 'released' and event.direction == "middle":
-          #permet de choisir entre enregistrer un a nouveau clef et message ou non si non, eteint le rasp
-          sense.show_message("Nouveau?",scroll_speed = 0.05)
-          while tourne :
-                  while conserver :
-                      sense.show_letter("V",(0, 255, 0))
-                      for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera conserve, une autre action proposera le x
-                          if event.action == 'pressed' and event.direction == "middle":
-                              tourne = False
-                              conserver = False 
-                              sense.clear()
-                              call("python3 encode_key.py", shell=True) #lance l encodage d une nouvelle clef
-                          if event.action == "pressed" and event.direction != "middle" :
-                              conserver = False
-                              sup = True
-                  while sup :
-                      sense.show_letter("X",(255, 0, 0))
-                      for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera supprime, une autre action recommencera la bouche
-                          if event.action == 'pressed' and event.direction == "middle":
-                              sup = False
-                              delete = False
-                              tourne = False
-                              f= open("message.txt","w") #ouvre le document message.txt
-                              f.write("") #remplace par un message vide donc supprime
-                              f.close()
-                              sense.clear()
-                              call("sudo shutdown now", shell=True) #stop le rasp
-                          if event.action == "pressed" and event.direction != "middle" :
-                              sup = False
-                              conserver = True
-      if event.action == "pressed" and event.direction != "middle" :
-        conserver = True
-        delete = False
-sense.clear()
