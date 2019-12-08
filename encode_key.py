f29c0e35a2cac154ab58142a498ec6071c964de0
EdouardDeWulf <e.dewulf@student.uclouvain.be>
2019-12-08 22:37:21 +0100

    implementation du module VX

--- encode_key.py
+++ encode_key.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat
 from subprocess import call
+import module 
 
 sense = SenseHat()
 sense.low_light = True
@@ -61,33 +62,14 @@ while tourne :
             liste_action.append(action) 
     if event.action == "held" and event.direction == "middle" :
         sense.show_message("Valider?",scroll_speed = 0.05)
-        conserver = True
-        validation = True
-        delete = False
-        while validation :
-            while conserver :
-                sense.show_letter("V",(0, 255, 0))
-                for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera conserve, une autre action proposera le x
-                    if event.action == 'pressed' and event.direction == "middle":
-                        tourne = False
-                        conserver = False
-                        validation = False
-                        sense.clear()
-                        f= open("key.txt","w") #ouvre le document message.txt
-                        f.write(hashing("".join(liste_action))) #ecrit la clef hashee
-                        f.close()
-                        call("python3 message.py", shell=True) #lance message.py
-                    if event.action == "pressed" and event.direction != "middle" :
-                        conserver = False
-                        delete = True
-            while delete :
-                sense.show_letter("X",(255, 0, 0))
-                for event in sense.stick.get_events(): # Si on valide en appuyant au milieu, le message sera supprime, une autre action recommencera la bouche
-                    if event.action == 'pressed' and event.direction == "middle":
-                        delete = False
-                        validation = False
-                        sense.clear()
-                        liste_action = []
-                    if event.action == "pressed" and event.direction != "middle" :
-                        conserver = True
-                        delete = False
+        a = module.vx() #VX est le return du choix entre V ou X
+        if a :
+            tourne = False 
+            f= open("key.txt","w") #ouvre le document message.txt
+            f.write(hashing("".join(liste_action))) #ecrit la clef hashee
+            f.close()
+            call("python3 message.py", shell=True) #lance message.py
+            
+        else :
+            liste_action = []
+            for event in sense.stick.get_events(): pass #reinitialise le compteur d actions
