3a25607e71b6f25cc4b9291291633baf49857de3
Manon <man.oreins@gmail.com>
2019-12-10 20:38:05 +0100

    Résolution du problême concernant la validation du dessin

--- demo.py
+++ demo.py
@@ -16,7 +16,7 @@ nothing = (0,0,0)
 list_color = [green,yellow,blue,red,nothing]
 tourne = True 
 
-dessin = [[0, 0, 0], [0, 0, 248], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
+dessin = [[0, 0, 0], [0, 0, 248], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \ #Dessin pour acceder a encode
 [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
 [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
 [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [248, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
@@ -25,7 +25,7 @@ dessin = [[0, 0, 0], [0, 0, 248], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0
 [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
 [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-dessin2 = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
+dessin2 = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \ #Dessin pour l auto destruction
 [0, 0, 0], [248, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [248, 0, 0], [0, 0, 0], \
 [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
 [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], \
@@ -43,8 +43,8 @@ previous_y = 0
 sense.set_pixel(x,y,white)
 
 def set_color(x,y) :
-    global color
-    sense.set_pixel(x,y,list_color[color])
+    global color #Utilise la variable globale color defini en dehors de la fonction
+    sense.set_pixel(x,y,list_color[color]) #Colore le pixel grace a la liste de couleurs
     color +=1
     if color == 5 :
         color = 0
@@ -57,13 +57,13 @@ while pro == True :
             x = 0
         color = 0
         col = sense.get_pixel(x,y)
-        if col == [0,0,0] :
+        if col == [0,0,0] : #Si non colore affiche un point blanc
             sense.set_pixel(x,y,white)
         else :
-            sense.set_pixel(x,y,white)
+            sense.set_pixel(x,y,white) #Fait clignoter le curseur sur les couleurs
             time.sleep(0.1)
             sense.set_pixel(x,y,col)
-        previous_x = x - 1
+        previous_x = x - 1 #Prend les coordonnees de la position precedente du curseur
         if previous_x == -1 :
             previous_x = 7
         previous_y = y
@@ -110,12 +110,12 @@ while pro == True :
         col = sense.get_pixel(x,y)
         if col == [0,0,0] :
             sense.set_pixel(x,y,white) #affiche le curseur en blanc
-        else : #permet de passer au dessus de couleur sans effacer
+        else : #permet de passer au dessus de couleur sans effacer, fait clignoter le curseur une fois sur les couleurs
             sense.set_pixel(x,y,white)
             time.sleep(0.1)
             sense.set_pixel(x,y,col)
-        previous_y = y+1
-        if previous_y == 8 :
+        previous_y = y + 1 #Prend les coordonnees de la position precedente
+        if previous_y == 8 : #Verfifie si ne depasse pas les bords
             previous_y = 0
         previous_x = x
         
@@ -129,31 +129,27 @@ while pro == True :
         sense.clear()
         
     if event.direction == "up" and event.action == "held" : #quand validation
-        pixel = sense.get_pixel(x,y)
-        if pixel != [248,252,248] : #La liste doit correspondre a blanc
-          sense.set_pixel(x,y,pixel) #efface pas la couleur
-        else : 
-          sense.set_pixel(x,y,nothing) #si c est blanc, efface le curseur
-        temp = sense.get_temperature() # quand pressed, prend la t.
+        pixel = sense.get_pixel(x,y) #Prend la couleur du pixel
+        if pixel == [248, 252, 248] : #Si le pixel est blanc, efface le curseur
+          sense.set_pixel(x,y,nothing)
+        temp = sense.get_temperature() #Prend la temp.
      
-        if sense.get_pixels() == dessin : #verifie si correspond au dessin secret
-            while event.action == "held" and event.direction == "up" :
-                #temps que le joystick est presse prend une deuxieme t.
-                tempb = sense.get_temperature()
-                tempc = sense.get_temperature_from_pressure()
-                if tempb > temp + 0.2 or tempc > temp + 0.2: #si la t augmente, affiche du rouge
-                    pro = False
-                    sense.clear(0, 255, 0)
-                    time.sleep(2)
-                    sense.clear()
-                    #Verifie s il y a un message enregistre, si oui lance le .py pour rentrer le mdp. S il n y en a pas lance message.py pour en rentrer un.
-                    f= open("message.txt","r") #ouvre le document message.txt
-                    message = f.read() #string message = contenu du doc
-                    f.close()
-                    if message == "" :
-                        import encode_key.py # Permet de rentrer la clef
-                    else :
-                        import decode_key.py # Demande la clef
+        if sense.get_pixels() == dessin and event.action == "held" and event.direction == "up" : #Tant que corresond au dessin secret et que le joystick est maintenu vers le haut
+            tempb = sense.get_temperature() #Prend une deuxieme temperature
+            tempc = sense.get_temperature_from_pressure()
+            if tempb > temp + 0.2 or tempc > temp + 0.2: #si la t augmente, affiche du rouge
+                pro = False #Arrete la boucle permettant de dessiner
+                sense.clear(0, 255, 0)
+                time.sleep(2)
+                sense.clear()
+                #Verifie s il y a un message enregistre, si oui lance le .py pour rentrer le mdp. S il n y en a pas lance message.py pour en rentrer un.
+                f= open("message.txt","r") #ouvre le document message.txt
+                message = f.read() #string message = contenu du doc
+                f.close()
+                if message == "" :
+                    import encode_key.py # Permet de rentrer la clef
+                else :
+                    import decode_key.py # Demande la clef
                         
                             
         elif sense.get_pixels() == dessin2 : #verifie si correspond au dessin secret /!\ kill switch
